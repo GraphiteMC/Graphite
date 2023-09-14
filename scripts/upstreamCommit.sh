bdd858f13c3a3c051e7a207c5807bf4c60e130b8
MrFishCakes <FinlayOxby@outlook.com>
2023-09-14 10:02:19 +0100

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@d527ade Updated Upstream (Paper)
    PurpurMC/Purpur@bd327ae Updated Upstream (Paper)
    PurpurMC/Purpur@7927f53 fix fire and lava immunity options, closes #1431

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -9,7 +9,8 @@
 set -e
 PS1="$"
 
-purpur=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PurpurMC/Purpur/compare/$1...HEAD | jq -r '.commits[] | "PurpurMC/Purpur@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+oldCommit=$(cat gradle.properties | grep previousCommit | sed 's/previousCommit = //')
+purpur=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PurpurMC/Purpur/compare/$oldCommit...HEAD | jq -r '.commits[] | "PurpurMC/Purpur@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
